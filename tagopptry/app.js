d05a4b5295803d45f3273b5d45eb22bb357843ff
Kaushik <kaushikdasroy@gmail.com>
2016-05-10 06:49:31 +0000

    /3rd commit

--- tagopptry/app.js
+++ tagopptry/app.js
@@ -2,5 +2,14 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT;
+
+app.get('/', function(req,res){
+    res.send('Hello World');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hello Routing');
+});
+
 console.log ('1-server is running on port :'+port);
 app.listen (port, function (err){ console.log ('App- server is running on port:'+ port)});
